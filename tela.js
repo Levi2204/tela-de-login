344809e076e7bb35d31dc956ba001ffc11c636bd
Levi2204 <leviteixeira225@gmail.com>
2023-05-22 17:19:37 -0300

    tela-de-login

---
+++ tela.js
@@ -0,0 +1,12 @@
+var a = window.document.getElementById ("botao")
+function clicar(){
+    window.location = "nubank.html"
+}
+function entrar(){
+    a.style.background = "rgb(126, 0, 126)"
+    a.style.color = "white"
+}
+function sair(){
+    a.style.background = "white"
+    a.style.color = "rgb(163, 163, 163)"
+}
